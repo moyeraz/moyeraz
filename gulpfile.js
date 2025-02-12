9174a91656d023810adad567478df6f3588c89ab
moyeraz <181419649+moyeraz@users.noreply.github.com>
2025-02-12 15:08:02 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
-const gulp = require('gulp');
-// const imagemin = require('gulp-imagemin');
-// const imageminWebp = require('imagemin-webp');
-const rename = require('gulp-rename');
+const gulp = require("gulp");
+const rename = require("gulp-rename");
 
-gulp.task('img-min', async () => {
+gulp.task("img-optimize", async () => {
+    const imagemin = (await import("gulp-imagemin")).default;
+
+    return gulp.src("public/**/*.{jpg,png}")
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 75, progressive: true }), // Compress JPG
+            imagemin.optipng({ optimizationLevel: 5 }) // Compress PNG
+        ]))
+        .pipe(gulp.dest("public/optimized/")); // Save optimized images
+});
+
+gulp.task("convert-webp", async () => {
     const imagemin = (await import("gulp-imagemin")).default;
     const imageminWebp = (await import("imagemin-webp")).default;
-    return gulp
-        .src(['public/**/*.{jpg,png}'], {encoding: false}, {base: './'})
-        .pipe(await imagemin(
-            [
-                imageminWebp({
-                    quality: 50
-                })
-            ]
-        ))
-        .pipe(rename({ extname: '.webp' }))
-        .pipe(gulp.dest('./public/'));
+
+    return gulp.src("public/**/*.{jpg,png}")
+        .pipe(imagemin([
+            imageminWebp({ quality: 50 }) // Convert to WebP with compression
+        ]))
+        .pipe(rename({ extname: ".webp" }))
+        .pipe(gulp.dest("public/webp/")); // Save WebP images separately
 });
+
+// Run both tasks
+gulp.task("default", gulp.series("img-optimize", "convert-webp"));
